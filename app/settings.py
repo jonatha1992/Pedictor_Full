6d699b364672c1f47c77f98c7de070e2e5388349
jonatha1992 <jonathancorrea291292@hotmail.com>
2024-08-26 00:13:53 -0300

    CORS ORIGINS

--- app/settings.py
+++ app/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# CORS_ALLOW_ALL_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://127.0.0.1:8000"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:8000"
+]
