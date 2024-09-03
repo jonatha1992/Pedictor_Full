207bf1e415d38cf5bd9522f74975963e41bc6006
jonatha1992 <jonathancorrea291292@hotmail.com>
2024-09-03 18:20:32 -0300

    colocacion de ruta front end

--- app/settings.py
+++ app/settings.py
@@ -140,7 +140,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "http://localhost:5173",
 ]
 
 
