466a0ed4ca9003b3bd05263d82c23c049cf6a532
jonathan-escritorio <joni92@gmail.com>
2025-01-16 00:10:34 -0300

    Eliminar conflictos de fusión en el registro de modelos en el admin de Django

--- backend/admin.py
+++ backend/admin.py
@@ -4,13 +4,7 @@ from backend.models import License, Report, User
 from backend.models import *
 
 # Register your models here.
-<<<<<<< HEAD
 
 admin.site.register(Report)
 admin.site.register(User)
 admin.site.register(License)
-=======
-admin.site.register(User)
-admin.site.register(Report)
-admin.site.register(License)
->>>>>>> main
