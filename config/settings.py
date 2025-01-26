d4deb83e326f1b0c090dca109fe15f3eb2422556
jonathan-escritorio <joni92@gmail.com>
2025-01-26 12:15:58 -0300

    Add python-dotenv dependency and configure environment variables for Django settings

--- config/settings.py
+++ config/settings.py
@@ -13,16 +13,18 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from datetime import timedelta
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kj=yn^+st@uel4#56_kc()k7zn=c*l6r2#gy%l41x4-i&bvb46'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
